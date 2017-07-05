d0791bef65297c06d4d4e533b39e62b23eb8cd1e
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-05 09:17:22 +0300

    no message

---
+++ T/Basic.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\ZohoInventory\T;
+// 2017-07-05
+final class Basic extends TestCase {
+	/** @test 2017-07-05 */
+	function t00() {}
+}
---
+++ T/TestCase.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\ZohoInventory\T;
+/**
+ * 2017-07-05
+ * @see \Dfe\ZohoInventory\T\Basic
+ */
+abstract class TestCase extends \Df\Zoho\T\TestCase {}
