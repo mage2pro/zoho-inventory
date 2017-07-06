afebdee041f49110036fb216f16cb8eec0436f6f
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-06 04:57:38 +0300

    0.0.5

--- API/Client.php
+++ API/Client.php
@@ -1,12 +1,12 @@
 <?php
 namespace Dfe\ZohoInventory\API;
 // 2017-07-05
-final class Client extends \Df\Zoho\API\BI\Client {
+final class Client extends \Df\ZohoBI\API\Client {
 	/**
 	 * 2017-07-06
 	 * @override
-	 * @see \Df\Zoho\API\BI\Client::version()
-	 * @used-by \Df\Zoho\API\BI\Client::uriBase()
+	 * @see \Df\ZohoBI\API\Client::version()
+	 * @used-by \Df\ZohoBI\API\Client::uriBase()
 	 * @return int
 	 */
 	protected function version() {return 1;}
--- T/Basic.php
+++ T/Basic.php
@@ -1,7 +1,7 @@
 <?php
 namespace Dfe\ZohoInventory\T;
 use Dfe\ZohoInventory\Settings as S;
-use Zend_Http_Client as C;
+use Dfe\ZohoInventory\API\Facade as F;
 // 2017-07-05
 final class Basic extends TestCase {
 	/** @test 2017-07-05 */
@@ -11,15 +11,5 @@ final class Basic extends TestCase {
 	function t01_token() {echo S::s()->token();}
 
 	/** @test 2017-07-05 */
-	function t02_organizations() {
-		/** @var C $c */
-		$c = new C;
-		$c->setConfig(['timeout' => 120]);
-		$c->setHeaders([
-			'Accept' => 'application/json'
-			,'Authorization' => 'Zoho-authtoken ' . S::s()->token()
-		]);
-		$c->setUri('https://inventory.zoho.com/api/v1/organizations');
-		echo df_json_prettify($c->request()->getBody());
-	}
+	function t02_organizations() {echo df_json_encode_pretty(F::organizations());}
 }
