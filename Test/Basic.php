d257287819bf057299ee33a69167b0073eb43703
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-11 03:55:55 +0000

    https://github.com/mage2pro/core/issues/168

--- Test/Basic.php
+++ Test/Basic.php
@@ -5,17 +5,17 @@ use Dfe\ZohoInventory\API\Facade as F;
 # 2017-07-05
 final class Basic extends TestCase {
 	/** 2017-07-05 @test */
-	function t00() {}
+	function t00():void {}
 
 	/** 2017-07-05 */
-	function t01_token() {print_r(S::s()->token());}
+	function t01_token():void {print_r(S::s()->token());}
 
 	/** 2017-07-05 */
-	function t02_organizations() {print_r(df_json_encode($this->f()->organizations()));}
+	function t02_organizations():void {print_r(df_json_encode($this->f()->organizations()));}
 
 	/** 2017-07-06 */
-	function t03_invalid() {print_r(df_json_encode($this->f()->p('invalid')));}
+	function t03_invalid():void {print_r(df_json_encode($this->f()->p('invalid')));}
 
 	/** 2017-07-07 @test */
-	function t04_currencies() {print_r(df_json_encode($this->f()->currencies()));}
+	function t04_currencies():void {print_r(df_json_encode($this->f()->currencies()));}
 }
